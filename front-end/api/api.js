11f6ae1cd310294eb558129fa5a6dbbb58569b57
juliadinizdti <julia.rodrigues@dtidigital.com.br>
2025-02-24 11:48:21 -0300

    COMMIT - Atualização URL

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import "dotenv/config";
 
-const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotify-replica-6sha.onrender.com/api";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSong = await axios.get(`${URL}/songs`);
